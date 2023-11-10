d2c0208b1a0ef0c7dc83d8a431b9a9590ba7ea67
BastianAsmussen <bastian@asmsusen.tech>
2023-11-10 12:43:41 +0100

    Create `print_clock(&RTC)` task.

---
+++ kernel/src/sys/task/clock.rs
@@ -0,0 +1,26 @@
+use crate::println;
+use crate::sys::time::rtc::RTC;
+use alloc::format;
+
+/// Print the clock on RTC update.
+///
+/// # Arguments
+///
+/// * `rtc` - The RTC struct.
+pub fn print_clock(rtc: &RTC) {
+    let date = format!(
+        "{day:02}/{month:02}/{century:02}{year:02}",
+        day = rtc.day,
+        month = rtc.month,
+        century = rtc.century,
+        year = rtc.year
+    );
+    let time = format!(
+        "{hour:02}:{minute:02}:{second:02}",
+        hour = rtc.hours,
+        minute = rtc.minutes,
+        second = rtc.seconds
+    );
+
+    println!("[INFO]: {date} @ {time}", date = date, time = time);
+}
