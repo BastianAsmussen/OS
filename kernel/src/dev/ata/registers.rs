326ee9883651d2e9dbb00422bb24c20323bbba12
BastianAsmussen <bastian@asmsusen.tech>
2023-11-14 14:52:32 +0100

    Merged `Register` enums.

--- kernel/src/dev/ata/registers.rs
+++ kernel/src/dev/ata/registers.rs
@@ -16,30 +16,23 @@ pub enum Direction {
 ///
 /// # Variants
 ///
-/// * `IO(IORegister)` - I/O registers.
+/// * `Data(u16)` - Data register, offset from I/O base by `0`.
+/// * `Error(u16)` - Error register, offset from I/O base by `1`.
+/// * `Features(u16)` - Features register, offset from I/O base by `1`.
+/// * `SectorCount(u16)` - Sector count register, offset from I/O base by `2`.
+/// * `SectorNumber(u16)` - Sector number register, offset from I/O base by `3`.
+/// * `CylinderLow(u16)` - Cylinder low register, offset from I/O base by `4`.
+/// * `CylinderHigh(u16)` - Cylinder high register, offset from I/O base by `5`.
+/// * `DriveHead(u16)` - Drive/head register, offset from I/O base by `6`.
+/// * `Status(u16)` - Status register, offset from I/O base by `7`.
+/// * `Command(u16)` - Command register, offset from I/O base by `7`.
 /// * `Control(ControlRegister)` - Control registers.
+///
+/// * `AlternateStatus(u16)` - Alternate status register, offset from control base by `0`.
+/// * `DeviceControl(u16)` - Device control register, offset from control base by `0`.
+/// * `DriveAddress(u16)` - Drive address register, offset from control base by `1`.
 #[derive(Debug)]
 pub enum Register {
-    IO(IORegister),
-    Control(ControlRegister),
-}
-
-/// I/O registers for ATA devices.
-///
-/// # Variants
-///
-/// * `Data(u16)` - Data register, offset by `0`.
-/// * `Error(u16)` - Error register, offset by `1`.
-/// * `Features(u16)` - Features register, offset by `1`.
-/// * `SectorCount(u16)` - Sector count register, offset by `2`.
-/// * `SectorNumber(u16)` - Sector number register, offset by `3`.
-/// * `CylinderLow(u16)` - Cylinder low register, offset by `4`.
-/// * `CylinderHigh(u16)` - Cylinder high register, offset by `5`.
-/// * `DriveHead(u16)` - Drive/head register, offset by `6`.
-/// * `Status(u16)` - Status register, offset by `7`.
-/// * `Command(u16)` - Command register, offset by `7`.
-#[derive(Debug)]
-pub enum IORegister {
     Data(u16),
     Error(u16),
     Features(u16),
@@ -50,15 +43,19 @@ pub enum IORegister {
     DriveHead(u16),
     Status(u16),
     Command(u16),
+
+    AlternateStatus(u16),
+    DeviceControl(u16),
+    DriveAddress(u16),
 }
 
-impl IORegister {
+impl Register {
     /// Returns the offset of the register.
     #[must_use]
     pub const fn offset(&self) -> u16 {
         match self {
-            Self::Data(base) => *base,
-            Self::Error(base) | Self::Features(base) => *base + 1,
+            Self::AlternateStatus(base) | Self::DeviceControl(base) | Self::Data(base) => *base,
+            Self::Error(base) | Self::Features(base) | Self::DriveAddress(base) => *base + 1,
             Self::SectorCount(base) => *base + 2,
             Self::SectorNumber(base) => *base + 3,
             Self::CylinderLow(base) => *base + 4,
@@ -78,42 +75,10 @@ impl IORegister {
             | Self::CylinderLow(_)
             | Self::CylinderHigh(_)
             | Self::DriveHead(_) => Direction::Both,
-            Self::Error(_) | Self::Status(_) => Direction::Read,
-            Self::Features(_) | Self::Command(_) => Direction::Write,
-        }
-    }
-}
-
-/// Control registers for ATA devices.
-///
-/// # Variants
-///
-/// * `AlternateStatus(u16)` - Alternate status register, offset by `0`.
-/// * `DeviceControl(u16)` - Device control register, offset by `0`.
-/// * `DriveAddress(u16)` - Drive address register, offset by `1`.
-#[derive(Debug)]
-pub enum ControlRegister {
-    AlternateStatus(u16),
-    DeviceControl(u16),
-    DriveAddress(u16),
-}
-
-impl ControlRegister {
-    /// Returns the offset of the register.
-    #[must_use]
-    pub const fn offset(&self) -> u16 {
-        match self {
-            Self::AlternateStatus(base) | Self::DeviceControl(base) => *base,
-            Self::DriveAddress(base) => *base + 1,
-        }
-    }
-
-    /// Returns the direction of the register.
-    #[must_use]
-    pub const fn direction(&self) -> Direction {
-        match self {
-            Self::AlternateStatus(_) | Self::DriveAddress(_) => Direction::Read,
-            Self::DeviceControl(_) => Direction::Write,
+            Self::Error(_) | Self::Status(_) | Self::AlternateStatus(_) | Self::DriveAddress(_) => {
+                Direction::Read
+            }
+            Self::Features(_) | Self::Command(_) | Self::DeviceControl(_) => Direction::Write,
         }
     }
 }
